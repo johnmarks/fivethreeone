d1e0d6d8c97cce6edc3985d0c896be852a1df2ba
John Marks <john@bluefroggaming.com>
2014-10-15 13:10:14 -0400

    get the project back to an empty state

--- app/views/workouts/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @workout, :id, :name, :created_at, :updated_at
--- db/migrate/20141014214129_create_workouts.rb
+++
@@ -1,9 +0,0 @@
-class CreateWorkouts < ActiveRecord::Migration
-  def change
-    create_table :workouts do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
