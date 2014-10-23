88f47302d183733668310110f4279638d67a2671
John Marks <john@bluefroggaming.com>
2014-10-23 11:26:32 -0400

    clean up the main workout page a bit

--- app/models/five_three_one_set_template.rb
+++ app/models/five_three_one_set_template.rb
@@ -2,6 +2,7 @@ class FiveThreeOneSetTemplate < SetTemplate
   def weight_for(user)
     orm = ExerciseData.where(:user_id => user.id, :exercise_id => exercise.id).first.working_one_rep_max
 
-    (orm * weight / 100.0).floor
+    w = (orm * weight / 100.0).floor / 5
+    w * 5
   end
 end
--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -2,12 +2,11 @@ class WorkoutSet < ActiveRecord::Base
   belongs_to :workout
   belongs_to :user
   delegate :day, :to => 'workout'
+  delegate :exercise, :to => 'workout'
+  attr_accessor :reps_completed
 
   def weight
     workout.weight_for(user)
   end
 
-  def to_s
-    "Exercise: #{workout.exercise}, Reps #{workout.reps}, Weight #{weight}"
-  end
 end
