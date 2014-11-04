19d2bb4b135bc0989e463c441518d58bfe4a1322
John Marks <john@bluefroggaming.com>
2014-11-04 15:34:59 -0500

    fixed a rounding bug, added my own data into seed data

--- app/models/five_three_one_set_template.rb
+++ app/models/five_three_one_set_template.rb
@@ -2,7 +2,7 @@ class FiveThreeOneSetTemplate < SetTemplate
   def weight_for(user)
     orm = user.exercise_datas.select{|d| d.exercise_id == exercise.id}.first.working_one_rep_max
    
-    w = (orm * weight / 100.0).floor / 5
-    w * 5
+    w = (orm * weight / 100.0).round * 2
+    w = w.round(-1)/2
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 program = Program.create! name: 'Five Three One', type: 'FiveThreeOneProgram'
 
-exercises = ['Deadlift', 'Overhead Press', 'Squat', 'Bench Press'].inject([]) do |inj, e|
+exercises = ['Bench Press', 'Deadlift', 'Overhead Press', 'Squat'].inject([]) do |inj, e|
 	inj << Exercise.create!(name: e)
 end
 
@@ -30,14 +30,62 @@ john = User.create email: 'john@bluefroggaming.com', password: 'mark1245', passw
 john.setup = true
 john.save!
 
-deadlift = Exercise.find_by_name('Deadlift')
-squat = Exercise.find_by_name('Squat')
 bench = Exercise.find_by_name('Bench Press')
+deadlift = Exercise.find_by_name('Deadlift')
 press = Exercise.find_by_name('Overhead Press')
+squat = Exercise.find_by_name('Squat')
 
-john.update_working_one_rep_max(deadlift.id, 300)
+john.update_working_one_rep_max(deadlift.id, 265)
 john.update_increment_ammount(deadlift.id, 10)
-john.update_working_one_rep_max(squat.id, 225)
+john.update_working_one_rep_max(squat.id, 210)
 john.update_increment_ammount(squat.id, 10)
-john.update_working_one_rep_max(bench.id, 225)
-john.update_working_one_rep_max(press.id, 140)
+john.update_working_one_rep_max(bench.id, 205)
+john.update_working_one_rep_max(press.id, 125)
+
+WorkoutManager.create_workout_sets(john, program)
+
+cycle = john.current_cycle
+
+cycle.each{|ws| ws.reps_completed = ws.reps}
+
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 5}.first.reps_completed = 10
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 5}.first.reps_completed = 12
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 5}.first.reps_completed = 10
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 5}.first.reps_completed = 8
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 3}.first.reps_completed = 7
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 3}.first.reps_completed = 7
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 3}.first.reps_completed = 10
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 3}.first.reps_completed = 7
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 1}.first.reps_completed = 6
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 1}.first.reps_completed = 9
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 1}.first.reps_completed = 6
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 1}.first.reps_completed = 5
+
+cycle.each{|ws| ws.finished = true; ws.save!}
+
+program.progress(john.exercise_datas)
+
+WorkoutManager.create_workout_sets(john, program)
+
+cycle = john.current_cycle
+cycle.each{|ws| ws.reps_completed = ws.reps}
+
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 5}.first.reps_completed = 6
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 5}.first.reps_completed = 10
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 5}.first.reps_completed = 10
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 5}.first.reps_completed = 7
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 3}.first.reps_completed = 5
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 3}.first.reps_completed = 8
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 3}.first.reps_completed = 8
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 3}.first.reps_completed = 4
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Bench Press'}.select{|wo| wo.reps == 1}.first.reps_completed = 4
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Deadlift'}.select{|wo| wo.reps == 1}.first.reps_completed = 6
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Overhead Press'}.select{|wo| wo.reps == 1}.first.reps_completed = 4
+cycle.select{|wo| wo.max_reps == true}.select{|wo| wo.exercise == 'Squat'}.select{|wo| wo.reps == 1}.first.reps_completed = 3
+
+cycle.each{|ws| 
+  ws.finished = true unless [13,14,15,16].include?(ws.day)  
+  ws.save!
+}
+
+
